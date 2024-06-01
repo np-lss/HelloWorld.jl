b05a414eb146c6d6609870582418d523bf586fef
Documenter.jl <documenter@juliadocs.github.io>
2024-06-01 22:32:13 +0000

    build based on 0155c64

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HelloWorld","category":"page"},{"location":"#HelloWorld","page":"Home","title":"HelloWorld","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HelloWorld.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HelloWorld]","category":"page"}]
+}
